37dfaab0c6a1efbcd536e2982a5d9ec127f7898b
Wilson Herrera <wilson.wilsoon81@gmail.com>
2026-07-21 21:59:27 -0500

    feat: implementar controladores y rutas para usuarios con Mongoose

---
+++ controllers/users.js
@@ -0,0 +1,46 @@
+const User = require('../models/user');
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch(() => {
+      res.status(500).send({ message: 'Error al obtener los usuarios' });
+    });
+};
+
+const getUserById = (req, res) => {
+  const { userId } = req.params;
+
+  User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: 'Usuario no encontrado' });
+      }
+      return res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'ID inválido' });
+      }
+      return res.status(500).send({ message: 'Error al obtener el usuario' });
+    });
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Datos inválidos' });
+      }
+      return res.status(500).send({ message: 'Error al crear el usuario' });
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,37 +1,12 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const { getUsers, getUserById, createUser } = require('../controllers/users');
 
 const router = express.Router();
-const usersPath = path.join(__dirname, '..', 'data', 'users.json');
 
-router.get('/', (req, res) => {
-  fs.readFile(usersPath, 'utf-8', (err, data) => {
-    if (err) {
-      res.status(500).send({ message: 'Error al leer los datos' });
-      return;
-    }
-    res.send(JSON.parse(data));
-  });
-});
+router.get('/', getUsers);
 
-router.get('/:_id', (req, res) => {
-  fs.readFile(usersPath, 'utf-8', (err, data) => {
-    if (err) {
-      res.status(500).send({ message: 'Error al leer los datos' });
-      return;
-    }
+router.get('/:userId', getUserById);
 
-    const users = JSON.parse(data);
-    const user = users.find((u) => u._id === req.params._id);
-
-    if (!user) {
-      res.status(404).send({ message: 'Usuario no encontrado' });
-      return;
-    }
-
-    res.send(user);
-  });
-});
+router.post('/', createUser);
 
 module.exports = router;
