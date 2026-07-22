26851db2e1b2353ef192ac029b59629161021a41
Wilson Herrera <wilson.wilsoon81@gmail.com>
2026-07-22 18:14:45 -0500

    fix: corregir middleware de express y conexion de mongoose, completar rutas de usuarios

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose');
 const app = express();
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/aroundb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/aroundb')
   .then(() => {
     console.log('✅ Conectado a la base de datos: aroundb');
   })
